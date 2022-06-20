bc58e0a32c4a2fbf2643041291dfae3cd3ca83f5
Isidora Lin <101031586+isi-lin@users.noreply.github.com>
2022-06-20 19:18:55 -0400

    Update main.js

--- App3JSG5/main.js
+++ App3JSG5/main.js
@@ -29,3 +29,35 @@ function findPaths(matriz, path,  i, j, salida, caminos, posicion, camino) {
     //obtengo los extremos de fila y columna
     let M = matriz.length;
     let N = matriz[0].length;
+// Si llegue al final imprimo el path
+    if (i === salida && j === matriz[0].length - 1) {
+        posicion.push(i)
+        posicion.push(j)
+        let pos = posicion.slice()
+        camino.push(pos)
+        let camino_copy = camino.slice()
+        caminos.push(camino_copy)
+        fs.writeFile('hola.txt', JSON.stringify(caminos), function (err) {
+            if (err) {
+                console.error('\x1b[31m', 'No se pudo ejecutar la aplicacion');
+            }
+        });
+        posicion.pop()
+        posicion.pop()
+        camino.pop()
+        return;
+    }
+
+    posicion.push(i)
+    posicion.push(j)
+    let pos = posicion.slice()
+    camino.push(pos)
+    path.push(`[${i},${j}]`);
+    //derecha
+    if ((i >= 0 && i < M && j + 1 >= 0 && j + 1 < N)) {
+        if(matriz[i][j+1] === 0 && getCamino(path,`[${i},${j+1}]`, `[${i},${j+1}]`) === false) {
+            posicion.pop()
+            posicion.pop()
+            findPaths(matriz, path, i, j + 1, salida, caminos, posicion, camino);
+        }
+    }
