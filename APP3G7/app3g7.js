1e327224ea21fc80f1e466840872eff4b0498695
Juanjo <42329001+juanjoayala01@users.noreply.github.com>
2022-06-25 16:52:56 -0400

    Function para encontrar el punto de partida

--- APP3G7/app3g7.js
+++ APP3G7/app3g7.js
@@ -1,1 +1,24 @@
+/**
+ * Esta funcion encuentra todos los caminos posibles
+ * moviendose en cuatro direcciones: arriba, abajo, izquierda y derecha
+ *
+ * @param mat matriz con valores
+ * @param path camino posible
+ * @param i pos de fila
+ * @param j pos de columna
+ * @param out array con los caminos posibles
+ */
 
+
+const {readFileSync, promises: fsPromises} = require('fs');
+let fs = require('fs');
+
+function find1stPos(mat, n) {
+    var fila = n;
+    if (mat[fila][0] == 0) {
+        return fila;
+    } else {
+        fila += 1;
+        return find1stPos(mat, fila);
+    }
+}
